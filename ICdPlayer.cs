81eae3d2f3823a8711bd5576f7034d9984c39fac
SefikaBoz <sefikaboz12@gmail.com>
2017-05-17 21:23:37 +0300

    Create ICdPlayer.cs

---
+++ ICdPlayer.cs
@@ -0,0 +1,23 @@
+# 17-05-2017---1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracts
+{
+    public interface ICdPlayer
+    {
+        void Play();
+        void Stop();
+        int FastForward(float numberOfSeconds);
+        int CurrentTrack { get; set;}
+        int MyProperty { get; set; }
+
+
+        
+
+    }
+}
